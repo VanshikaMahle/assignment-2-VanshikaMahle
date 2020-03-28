92c1d9c242d54b6c8e92f557c416c107d2ce6341
VanshikaMahle <vanshika.mahle_cs18@gla.ac.in>
2020-03-28 15:12:35 +0530

    mymain3

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -13,12 +13,12 @@ import problem3.student.Student;
 public class MyMain {
     public static void main(String[] args) {
         MyPriorityQueue queue = new MyPriorityQueue();
-        queue.enqueue(new Student("Tushar", 58));
-        queue.enqueue(new Student("Tanishq", 23));
-        queue.enqueue(new Student("Pavit", 65));
-        queue.enqueue(new Student("Samar", 1));
-        queue.enqueue(new Student("Vibhu", 10));
-        queue.enqueue(new Student("Shantanu", 76));
+        queue.enqueue(new Student("vanshika", 10));
+        queue.enqueue(new Student("kirti", 20));
+        queue.enqueue(new Student("manya", 30));
+        queue.enqueue(new Student("riyansh", 40));
+        queue.enqueue(new Student("deepika", 50));
+        queue.enqueue(new Student("divy", 60));
 
         queue.show();
         queue.dequeue();
